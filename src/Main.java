e98dd90dc2ae5ea1824ebb6e925fc82d43ae0410
Stepanov Ivan <ivan.stepanov.05@list.ru>
2024-05-01 16:46:20 +0700

    seventh work

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.InputMismatchException;
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.Map;
+import java.util.OptionalDouble;
 import java.util.Scanner;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * The `Main` class contains the main method to execute the program. It provides a menu-driven
@@ -12,38 +22,63 @@ import java.util.logging.Logger;
  * exit the program.
  */
 public class Main {
+  private static final Scanner scanner = new Scanner(System.in);
+  private static ArrayList<Person> people = new ArrayList<>();
   private static final Logger logger = Logger.getLogger(Main.class.getName());
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    ArrayList<Person> people = new ArrayList<>();
     ClassLoader.getSystemClassLoader().setDefaultAssertionStatus(true);
+    String fileName = "people.dat";
 
     boolean isRunning = true;
     while (isRunning) {
-      System.out.println("\nMenu:");
-      System.out.println("1. Add empty person");
-      System.out.println("2. Add person with data");
-      System.out.println("3. Edit person's field");
-      System.out.println("4. Display all people");
-      System.out.println("5. Sort people by...");
-      System.out.println("6. Weight to normal");
-      System.out.println("7. Exit");
-      System.out.print("Enter your choice: ");
+      System.out.println(
+        """
+        Menu:
+        1. Load data from file
+        2. Save data in file
+        3. Add Empty person
+        4. Add person with data
+        5. Edit person's field
+        6. Display all people
+        7. Sort people by...
+        8. Weight to normal
+        9. Filter by field
+        10. Removing duplicates from an array
+        11. Total growth of objects
+        12. Age statistics when grouped by gender
+        13. Exit
+        Enter your choice:\s"""
+      );
       try {
         int index = 0;
-        int choice = scanner.nextInt();
+        String choice = scanner.nextLine();
         Person person;
 
         switch (choice) {
-          case 1:
-            people.add(new Person());
+          case "1":
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
+              people = (ArrayList<Person>) ois.readObject();
+              System.out.println("The list of people has been successfully uploaded from the file:");
+            } catch (IOException | ClassNotFoundException e) {
+              System.err.println("Error loading the list from a file: " + e.getMessage());
+            }
             break;
-          case 2:
+          case "2":
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
+              oos.writeObject(people);
+              System.out.println("The list of people has been successfully saved to a file.");
+            } catch (IOException e) {
+              System.err.println("Error saving the list to a file: " + e.getMessage());
+            }
+            break;
+          case "3":
+            people.add(new Person(people.size()));
+            System.out.println("Empty person has been entered");
+            break;
+          case "4":
             double weight, height;
             int age;
-            scanner.nextLine(); // Consume newline
-
             try {
               System.out.print("Enter name: ");
               String name = scanner.nextLine();
@@ -64,21 +99,23 @@ public class Main {
               System.out.print("Enter white or not(true, false): ");
               boolean whiteOrNot = scanner.nextBoolean();
 
-              person = new Person(name, weight, age, gender, height, whiteOrNot);
+              person = new Person(name, weight, age, gender, height, whiteOrNot, people.size());
               people.add(person);
             } catch (EmptyStringException | NegativeValueException e) {
               throw new RuntimeException("Error when input parameters", e);
             } catch (AssertionError e) {
               throw new RuntimeException("Error when input gender");
+            } finally{
+              scanner.nextLine();
             }
             System.out.println("Values have been entered");
             break;
-          case 3:
+          case "5":
             System.out.print("Enter index of person to edit: ");
             index = scanner.nextInt();
+            scanner.nextLine();
             if (index >= 0 && index < people.size()) {
               person = people.get(index);
-              scanner.nextLine(); // Consume newline
               System.out.print("Enter field to edit (name/weight/age/gender/height/whiteOrNot): ");
               String field = scanner.nextLine();
               try {
@@ -120,55 +157,58 @@ public class Main {
               System.out.println("Invalid index!");
             }
             break;
-          case 4:
+          case "6":
             if (people.isEmpty()) {
               System.out.println("No people");
               break;
             }
-            for (Person persona : people) {
-              persona.displayInfo(index);
-              System.out.println();
-              index++;
-            }
+            Stream<Person> listStream = people.stream();
+            listStream.forEach(System.out::println);
+            listStream.close();
             break;
-          case 5:
+          case "7":
             if (people.isEmpty()) {
               System.out.println("No people");
               break;
             }
-            System.out.print("Enter field to sort by (name/weight/age/gender/height/whiteOrNot): ");
-            scanner.nextLine();
+            System.out.print("Enter field to sort by (name/weight/age/gender/height/isWhite): ");
             String field = scanner.nextLine();
             switch (field) {
               case "name":
                 people.sort(Comparator.comparing(Person::getName));
+                setIndexes();
                 System.out.println("People sorted by name:");
                 break;
               case "weight":
                 people.sort(Comparator.comparing(Person::getWeight));
+                setIndexes();
                 System.out.println("People sorted by weight:");
                 break;
               case "age":
                 people.sort(Comparator.comparing(Person::getAge));
+                setIndexes();
                 System.out.println("People sorted by age:");
                 break;
               case "gender":
                 people.sort(Comparator.comparing(Person::getGender));
+                setIndexes();
                 System.out.println("People sorted by gender:");
                 break;
               case "height":
                 people.sort(Comparator.comparingDouble(Person::getHeight));
+                setIndexes();
                 System.out.println("People sorted by height:");
                 break;
-              case "whiteOrNot":
+              case "isWhite":
                 people.sort(Comparator.comparing(Person::getIsWhite));
+                setIndexes();
                 System.out.println("People sorted by white or not");
                 break;
               default:
                 System.out.println("Invalid field!");
             }
             break;
-          case 6:
+          case "8":
             System.out.print("Enter index of person to know weight to normal: ");
             index = scanner.nextInt();
             if (index >= 0 && index < people.size()) {
@@ -179,7 +219,23 @@ public class Main {
               System.out.println("Invalid index!");
             }
             break;
-          case 7:
+          case "9":
+            personFilter();
+            scanner.nextLine();
+            break;
+          case "10":
+            people = (ArrayList<Person>) people.stream().distinct().collect(Collectors.toList());
+            setIndexes();
+            people.forEach(System.out::println);
+            System.out.println("Duplicates have been deleted");
+            break;
+          case "11":
+            sumHeight();
+            break;
+          case "12":
+            createGroup();
+            break;
+          case "13":
             isRunning = false;
             break;
           default:
@@ -193,6 +249,72 @@ public class Main {
       }
     }
     scanner.close();
+  }
+
+  public static void setIndexes(){
+    for(int i = 0; i < people.size(); i++){
+      people.get(i).setIndex(i);
+    }
+  }
+
+  /** The method for filtering by the selected field */
+  public static void personFilter() {
+    System.out.println(
+      """
+      Select the field to filter:
+      1. Age
+      2. Weight
+      """);
+    String choice = scanner.nextLine();
+    Stream<Person> personList = people.stream();
+    int value;
+    switch (choice) {
+      case "1":
+        System.out.println("Enter the lower age limit");
+        value = scanner.nextInt();
+        Stream<Person> filterPerson = personList.filter(p -> p.getAge() >= value);
+        filterPerson.forEach(System.out::println);
+        break;
+      case "2":
+        System.out.println("Enter the lower weight limit");
+        value = scanner.nextInt();
+        Stream<Person> filterList = personList.filter(p -> p.getWeight() >= value);
+        filterList.forEach(System.out::println);
+        break;
+      default:
+        System.out.println("Incorrect choice");
+    }
+  }
+
+  /** The method calculates the sum of all the growth of all people and outputs it to the console */
+  public static void sumHeight() {
+    Stream<Person> listPerson = people.stream();
+    OptionalDouble result = listPerson.mapToDouble(Person::getHeight).reduce(Double::sum);
+    if (result.isPresent()) {
+      System.out.println("The total growth of all people: " + result.getAsDouble());
+    } else {
+      System.out.println("No people");
+    }
+    listPerson.close();
+  }
+
+  /** The method groups the data by gender and outputs the average age values */
+  public static void createGroup() {
+    Map<String, IntSummaryStatistics> summaryStatisticsByGender =
+            people.stream()
+                    .collect(
+                            Collectors.groupingBy(
+                                    Person::getGender, Collectors.summarizingInt(Person::getAge)));
+
+    summaryStatisticsByGender.forEach(
+            (gender, stats) -> {
+              System.out.println("Gender " + gender + ":");
+              System.out.println("Number of people: " + stats.getCount());
+              System.out.println("Average age: " + (int) stats.getAverage());
+              System.out.println("Total age: " + stats.getSum());
+              System.out.println("Min age: " + stats.getMin());
+              System.out.println("Max age: " + stats.getMax());
+            });
   }
 
   /**
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,4 @@
+import java.io.Serializable;
 import java.util.Objects;
 
 /**
@@ -7,7 +8,8 @@ import java.util.Objects;
  * includes a method to calculate the difference between the person's current weight and the ideal
  * weight based on height, age, and gender.
  */
-class Person {
+class Person implements Serializable {
+  private int index;
   private String name;
   private double weight;
   private double height;
@@ -15,9 +17,14 @@ class Person {
   private String gender;
   private boolean isWhite;
 
-  // Default constructor
-  public Person() {
+  /**
+   * Default constructor for the Person class.
+   *
+   * @param index the index of the person
+   */
+  public Person(int index) {
     try {
+      setIndex(index);
       setName("Noname");
       setWeight(0.0);
       setAge(0);
@@ -28,9 +35,23 @@ class Person {
     }
   }
 
-  // Default constructor
-  public Person(String name, double weight, int age, String gender, double height, boolean isWhite)
+  /**
+   * Constructor with parameters for the Person class.
+   *
+   * @param name the name of the person
+   * @param weight the weight of the person
+   * @param age the age of the person
+   * @param gender the gender of the person
+   * @param height the height of the person
+   * @param isWhite whether the person is white or not
+   * @param index the index of the person
+   * @throws EmptyStringException if the name is empty
+   * @throws NegativeValueException if weight, age, or height is negative
+   */
+  public Person(
+      String name, double weight, int age, String gender, double height, boolean isWhite, int index)
       throws EmptyStringException, NegativeValueException {
+    setIndex(index);
     setName(name);
     setWeight(weight);
     setAge(age);
@@ -101,15 +122,52 @@ class Person {
     this.isWhite = isWhite;
   }
 
-  // Method to display information about the person
-  public void displayInfo(int index) {
-    System.out.println("Index: " + index);
-    System.out.println("Name: " + name);
-    System.out.println("Weight: " + weight);
-    System.out.println("Age: " + age);
-    System.out.println("Gender: " + gender);
-    System.out.println("Height: " + height);
-    System.out.println("isWhite: " + isWhite);
+  public void setIndex(int index) {
+    this.index = index;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Person person = (Person) o;
+    return age == person.age
+        && Objects.equals(name, person.name)
+        && weight == person.weight
+        && height == person.height
+        && Objects.equals(gender, person.gender)
+        && isWhite == person.isWhite;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, age, weight, height, gender, isWhite);
+  }
+
+  @Override
+  public String toString() {
+    return "Index: "
+        + index
+        + "\n"
+        + "Name: "
+        + name
+        + "\n"
+        + "Weight: "
+        + weight
+        + "\n"
+        + "Age: "
+        + age
+        + "\n"
+        + "Gender: "
+        + gender
+        + "\n"
+        + "Height: "
+        + height
+        + "\n"
+        + "Is white?: "
+        + isWhite
+        + "\n";
   }
 
   /**
